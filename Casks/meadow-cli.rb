2905c2d37297b9e1c5b68999dff6407aed729e41
Gordon McCreight <gordon@sandharbor.co>
2026-08-28 17:05:40 -0700

    Release Meadow Command 0.5.49

--- Casks/meadow-cli.rb
+++ Casks/meadow-cli.rb
@@ -1,13 +1,13 @@
 cask "meadow-cli" do
-  version "0.5.45"
-  sha256 "224ca6ff2a89d2fe17c80384d6d50a50cdf5bdd1b73847c01f2dec499b0ca82e"
+  version "0.5.49"
+  sha256 "ac81af9037f659174e17088841b9d59a1b0f50c62aab4d2708ebb26493ec18ee"
 
-  url "https://meadowshare.com/app/dist/Meadow-Command-0.5.45-darwin-arm64.zip"
+  url "https://meadowshare.com/app/dist/Meadow-Command-0.5.49-darwin-arm64.zip"
   name "Meadow Command"
   desc "Command-line client for Meadow"
   homepage "https://github.com/sandharbor/meadow"
 
   depends_on arch: :arm64
 
-  binary "Meadow-Command-0.5.45-darwin-arm64/bin/meadow", target: "meadow"
+  binary "Meadow-Command-0.5.49-darwin-arm64/bin/meadow", target: "meadow"
 end
