08d1e887d836fc8102caf47c179263645a2e8425
Gordon McCreight <gordon@sandharbor.co>
2026-09-04 12:59:50 -0700

    Update Meadow Command prerelease to 0.5.52-rc-20260904-195051

--- Casks/meadow-cli@prerelease.rb
+++ Casks/meadow-cli@prerelease.rb
@@ -1,13 +1,13 @@
 cask "meadow-cli@prerelease" do
-  version "0.5.51-rc-20260904-170500"
-  sha256 "790f0f2669174b4bd13f884f2ebf8df04dbafe04d67e1663e365cdb0a899ff9c"
+  version "0.5.52-rc-20260904-195051"
+  sha256 "aeaf710f6f12323f9157c4211ab0a574b8a10a21194cfa2b06fead3f20ec52f3"
 
-  url "https://meadowshare.com/app/dist/Meadow-Command-0.5.51-darwin-arm64-rc-20260904-170500.zip"
+  url "https://meadowshare.com/app/dist/Meadow-Command-0.5.52-darwin-arm64-rc-20260904-195051.zip"
   name "Meadow Command (Prerelease)"
   desc "Prerelease Homebrew installation of the Meadow command-line client"
   homepage "https://github.com/sandharbor/meadow"
 
   depends_on arch: :arm64
 
-  binary "Meadow-Command-0.5.51-darwin-arm64/bin/meadow", target: "meadow"
+  binary "Meadow-Command-0.5.52-darwin-arm64/bin/meadow", target: "meadow"
 end
